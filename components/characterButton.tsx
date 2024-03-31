e9405f867b103591b3454721e10d92f464b89537
Jason Shang <jasonshang2002@gmail.com>
2024-03-31 10:29:43 -0400

    button & chat box UI

---
+++ components/characterButton.tsx
@@ -0,0 +1,18 @@
+// each character serves as a button to open up the corresponding chat box
+"use client"
+import {useState} from 'react';
+import ChatBox from '@/components/chatbox';
+import {Button} from '@/components/ui/button';
+
+export default function CharacterButton() {
+    const [chatBoxOpen, setChatBoxOpen] = useState(false);
+
+    return (
+        <>
+            <Button onClick={() => setChatBoxOpen(true)} >
+                Alyosha Karamazov
+            </Button>
+            <ChatBox open={chatBoxOpen} onClose={() => setChatBoxOpen(false)} />
+        </>
+    );
+}
---
+++ components/chatbox.tsx
@@ -0,0 +1,42 @@
+import {useChat} from 'ai/react';
+import {cn} from '@/lib/utils';
+import {Button} from '@/components/ui/button';
+import {Form} from '@/components/ui/form';
+import {Input} from '@/components/ui/input';
+import { XCircle } from 'lucide-react';
+
+interface ChatBoxProps {
+    open: boolean;
+    onClose: () => void;
+}
+
+export default function ChatBox({open, onClose}: ChatBoxProps) {
+    const {
+        messages,
+        input,
+        handleInputChange,
+        handleSubmit,
+        setMessages,
+        isLoading,
+        error
+    } = useChat();
+
+    return (
+        <div className={cn("bottom-0 right-0 z-10 w-full max-w-[500px] xl:right-36", open ? "fixed" : "hidden")}>
+            <button onClick={onClose} className="mb-1 ms-auto block">
+                <XCircle size={30}/>
+            </button>
+            <div className="flex h-[600px] flex-col rounded bg-background border shadow-xl">
+                <div className="h-full">Messages</div>
+                <form onSubmit={handleSubmit} className="m-3 flex gap-1">
+                    <Input
+                        value={input}
+                        onChange={handleInputChange}
+                        placeholder="Ask anything!"
+                    />
+                    <Button type="submit">Submit</Button>
+                </form>
+            </div>
+        </div>
+    );
+}
