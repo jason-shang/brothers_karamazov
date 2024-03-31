95a410b67ad5f7032b7ea1e0111939ca3e63082c
Jason Shang <jasonshang2002@gmail.com>
2024-03-31 10:36:19 -0400

    fixed type error

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
       </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <CharacterButton>Alyosha Karamazov</CharacterButton>
+        <CharacterButton />
       </div>
     </main>
   );
